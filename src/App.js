e176dc3efdeda1b236391bca88dbba59c290e56d
Dominique Nguyen <domnguyen@gmail.com>
2019-03-24 18:46:58 -0700

    header added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import HeadWrapper from './components/HeadWrapper'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <React.Fragment>
+        <div id="preloader">
+          <div id="status">
+            <img src="images/header/preloader.gif" id="preloader_image" alt="loader" />
+	        </div>
+        </div>
+        <HeadWrapper />
+        </React.Fragment>
     );
   }
 }
