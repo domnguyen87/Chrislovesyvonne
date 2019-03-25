d988dc95ac04bad3cb49b92ef322a9fba0e88120
Dominique Nguyen <domnguyen@gmail.com>
2019-03-24 21:23:52 -0700

    added slides

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-// import './App.css';
-import HeadWrapper from './components/HeadWrapper'
+import HeadWrapper from "./components/HeadWrapper"
+import RevolutionSlider from './components/RevolutionSlider'
 
 class App extends Component {
   render() {
     return (
         <React.Fragment>
-        <div id="preloader">
-          <div id="status">
-            <img src="images/header/preloader.gif" id="preloader_image" alt="loader" />
-	        </div>
-        </div>
-        <HeadWrapper />
+          <div id="preloader">
+            <div id="status">
+              <img src="images/header/preloader.gif" id="preloader_image" alt="loader" />
+	          </div>
+          </div>
+          {/* <!-- Header Wrapper Start --> */}
+          <HeadWrapper />
+          {/* <RevolutionSlider /> */}
+
         </React.Fragment>
     );
   }
--- src/components/HeadWrapper.jsx
+++ src/components/HeadWrapper.jsx
@@ -3,42 +3,43 @@ import React from 'react'
 class HeadWrapper extends React.Component {
     render() {
         return (
-            <div section-scroll='0' className="wd_scroll_wrap">
-                <div className="wd_slider_wrapper wd_single_index_menu">
-                    <div className="wd_header_wrapper">
-                        <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 visible-xs visible-sm">
-                            <div className="wd_logo">
-                                <img src="images/header/logo.png" alt="Logo" title="Logo" className="img-responsive" />
-                                    <button className="wd_menu_btn"><i className="fa fa-bars" aria-hidden="true"></i></button>
-			                </div>
-                        </div>
-                        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div className="wd_mainmenu_wrapper">
-                                <div className="wd_main_menu_wrapper">
-                                    <div className="wd_main_menu wd_single_index_menu">
-                                        <ul>
-                                            <li><a href="0">Home</a></li>
-                                            <li><a href="1">About Us</a></li>
-                                            <li><a href="2">Love Story</a></li>
-                                            <li><a href="3">Events</a></li>
-                                            <li className="hidden-xs hidden-sm"><a href="#">
-                                                <img id="logo-navbar-middle" src="images/header/logo2.png" className="img-responsive" alt="logo" /></a></li>
-                                            <li><a href="4">Rsvp</a></li>
-                                            <li><a href="5">Family</a></li>
-                                            <li><a href="6">Gallery</a></li>
-                                            <li><a href="7">Blog</a></li>
-                                            <li><a href="8">Contact Us</a></li>
-                                        </ul>
+            <React.Fragment>
+                <div section-scroll='0' className="wd_scroll_wrap">
+                    <div className="wd_slider_wrapper wd_single_index_menu">
+                        <div className="wd_header_wrapper">
+                            <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 visible-xs visible-sm">
+                                <div className="wd_logo">
+                                    <img src="images/header/logo.png" alt="Logo" title="Logo" className="img-responsive" />
+                                        <button className="wd_menu_btn"><i className="fa fa-bars" aria-hidden="true"></i></button>
+			                    </div>
+                                </div>
+                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div className="wd_mainmenu_wrapper">
+                                        <div className="wd_main_menu_wrapper">
+                                            <div className="wd_main_menu wd_single_index_menu">
+                                                <ul>
+                                                    <li><a href="0">Home</a></li>
+                                                    <li><a href="1">About Us</a></li>
+                                                    <li><a href="2">Love Story</a></li>
+                                                    <li><a href="3">Events</a></li>
+                                                    <li className="hidden-xs hidden-sm"><a href="#">
+                                                        <img id="logo-navbar-middle" src="images/header/logo2.png" className="img-responsive" alt="logo" /></a></li>
+                                                    <li><a href="4">Rsvp</a></li>
+                                                    <li><a href="5">Family</a></li>
+                                                    <li><a href="6">Gallery</a></li>
+                                                    <li><a href="7">Blog</a></li>
+                                                    <li><a href="8">Contact Us</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        </div>
                     </div>
-                </div>
+            </React.Fragment>
         )
     }
-
 }
 
 export default HeadWrapper
